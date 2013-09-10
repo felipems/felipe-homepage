4a553cc8626cb01af26cbfb206f0b9ecc837d5eb
Felipe Munera <felipems@Felipes-MacBook-Pro.local>
2013-09-09 21:32:12 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FelipeHomepage::Application.config.secret_token = '73466ecc1f533714323229ef823680f75fed854b17e532c86867ac75ee24871a0ad3f10d5bff6c1c608b0d8e0c881d7a720f9ed58dbe31d4403859753ceb9a30'
